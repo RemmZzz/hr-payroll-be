0b26267eba14847903b176c6834d8c853ca19849
MinhBui <buivanremdh@gmail.com>
2025-11-29 15:00:45 +0700

    khoi tao du an

---
+++ src/main/java/com/edutech/hrpayrollbe/HrPayrollBeApplication.java
@@ -0,0 +1,13 @@
+package com.edutech.hrpayrollbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrPayrollBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrPayrollBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edutech/hrpayrollbe/HrPayrollBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edutech.hrpayrollbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
